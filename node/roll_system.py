f73d44d712741e2cfdc6d249e2333b4af1a60c6f
ray <ray@dagon>
2012-07-10 22:52:42 +0100

    Adding default roll system

--- node/roll_system.py
+++ node/roll_system.py
@@ -1,3 +1,8 @@
 
-def roll(actor, stats):
-    pass
+import random
+
+def roll(actor, stats, target):
+    result = random.randint(1, 20)
+    for stat in stats:
+        result += actor.get_stat(stat)
+    return result >= target
