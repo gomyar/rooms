00c1973a3b29e6e22cff6899bf040a478c5f3c27
ray <ray@dagon>
2013-01-13 21:22:05 +0000

    Adding item_registry class

---
+++ src/rooms/item_registry.py
@@ -0,0 +1,14 @@
+
+class ItemRegistry(object):
+    def __init__(self):
+        self._items = dict()
+
+    def add_item(self, item):
+        self._items[item.item_type] = item
+
+    def get_item(self, item_type):
+        return self._items[item_type]
+
+    def items(self, category=""):
+        return [item for item in self._items.values() if \
+            item.category == category]
