57ef6b9739b6748c480b139a32ec266616bf0fc2
ray <ray@dagon>
2014-05-04 17:57:50 +0100

    Adding linearopen geography

--- src/rooms/waypoint.py
+++ src/rooms/waypoint.py
@@ -1,11 +1,110 @@
 
+import time
+import math
+
+from rooms.timing import get_now
+
+import logging
+log = logging.getLogger("rooms.path")
+
+
+def distance(x1, y1, x2, y2):
+    return math.hypot(x2 - x1, y2 - y1)
+
+
+def path_from_waypoints(point_list, speed):
+    path = []
+    last_x, last_y = point_list[0]
+    current_time = get_now()
+    path.append( (last_x, last_y, current_time ) )
+    for point in point_list[1:]:
+        x, y = point
+        current_time += distance(last_x, last_y, x, y) / speed
+        path.append( (x, y, current_time ) )
+        last_x, last_y = x, y
+    return Path(path)
+
 
 class Path(object):
     def __init__(self, waypoints=None):
-        self.waypoints = waypoints or []
+        self.path = waypoints or []
 
     def __repr__(self):
-        return "<Path: %s>" % ([p for p in self.waypoints],)
+        return "Path(%s)" % (self.path,)
 
     def __eq__(self, rhs):
-        return rhs and type(rhs) == Path and self.waypoints == rhs.waypoints
+        return rhs and type(rhs) is Path and self.path == rhs.path
+
+    def set_position(self, position):
+        self.path = [ (position, get_now() ), (position, get_now() ) ]
+
+    def path_end_time(self):
+        return self.path[-1][2]
+
+    def path_end_position(self):
+        return self.path[-1][:2]
+
+    def basic_path_list(self):
+        return [(p[0], p[1]) for p in self.path]
+
+    def position(self):
+        return (self.x(), self.y())
+
+    def speed(self):
+        now = get_now()
+        path = list(self.path)
+        start = path.pop(0)
+        while path and now > path[0][2]:
+            start = path.pop(0)
+        if len(path) > 0:
+            end = path[0]
+            start_x, start_y, start_time = start
+            end_x, end_y, end_time = end
+            if now > end_time:
+                return 0
+            else:
+                dist = distance(start_x, start_y, end_x, end_y)
+                taken = end_time - start_time
+                return dist / taken if taken else 0
+        else:
+            return 0
+
+    def x(self):
+        now = get_now()
+        path = list(self.path)
+        start = path[0]
+        while path and now > path[0][2]:
+            start = path.pop(0)
+        if not path:
+            return self.path[-1][0]
+        start_x, start_y, start_time = start
+        end_x, end_y, end_time = path[0]
+
+        if now > end_time:
+            return end_x
+        diff_x = end_x - start_x
+        diff_t = end_time - start_time
+        if diff_t <= 0:
+            return end_x
+        inc = (now - start_time) / diff_t
+        return start_x + diff_x * inc
+
+    def y(self):
+        now = get_now()
+        path = list(self.path)
+        start = path[0]
+        while path and now > path[0][2]:
+            start = path.pop(0)
+        if not path:
+            return self.path[-1][1]
+        start_x, start_y, start_time = start
+        end_x, end_y, end_time = path[0]
+
+        if now > end_time:
+            return end_y
+        diff_y = end_y - start_y
+        diff_t = end_time - start_time
+        if diff_t <= 0:
+            return end_y
+        inc = (now - start_time) / diff_t
+        return start_y + diff_y * inc
