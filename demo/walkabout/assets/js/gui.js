2b03686966319a43d0d5420dcea47199607f9984
ray <ray@dagon>
2014-05-27 21:16:16 +0100

    Adding drawing room objects

--- demo/walkabout/assets/js/gui.js
+++ demo/walkabout/assets/js/gui.js
@@ -82,70 +82,14 @@ gui.draw = function()
     gui.redraw_timeout = null;
     gui.ctx.clearRect(0, 0, gui.canvas.width, gui.canvas.height);
 
-    gui.draw_room();
-    gui.draw_actors();
+    gui_room.draw_room();
+    gui_actors.draw_actors();
 
     gui.draw_debug();
 
     var nowish = api_rooms.get_now();
     if (nowish < gui.redraw_until)
         gui.requestRedraw();
-}
-
-gui.draw_room = function()
-{
-    gui.ctx.strokeStyle = "rgb(0, 255, 255)";
-    var x = api_rooms.room.topleft.x;
-    var y = api_rooms.room.topleft.y;
-    var width = api_rooms.room.bottomright.x - x;
-    var height = api_rooms.room.bottomright.y - y;
-    gui.ctx.strokeRect(gui.canvas_x(x), gui.canvas_y(y), width / gui.zoom, height / gui.zoom)
-
-    // Draw room objects
-    for (object_id in api_rooms.room.room_objects)
-    {
-        var room_object = api_rooms.room.room_objects[object_id];
-        var x = room_object.topleft.x;
-        var y = room_object.topleft.y;
-        var width = room_object.bottomright.x - x;
-        var height = room_object.bottomright.y - y;
-        gui.ctx.drawImage(guiassets.images[room_object.object_type], gui.canvas_x(x), gui.canvas_y(y), width / gui.zoom, height / gui.zoom);
-    }
-}
-
-gui.draw_actors = function()
-{
-    // Draw actors
-    for (var i in api_rooms.actors)
-    {
-        var actor = api_rooms.actors[i];
-        gui.ctx.save();
-        gui.ctx.translate(gui.canvas_x(actor.x()), gui.canvas_y(actor.y()));
-        gui.draw_actor(actor);
-        gui.ctx.restore();
-    }
-}
-
-gui.draw_player_actor = function(ctx, actor)
-{
-    var img = guiassets.images[actor.model_type];
-    var width = img.width / gui.zoom;
-    var height = img.width / gui.zoom;
-    ctx.rotate(Math.atan2(actor.vector.end_pos.y - actor.vector.start_pos.y,
-        actor.vector.end_pos.x - actor.vector.start_pos.x));
-    var offset = 0;
-    if (actor.vector.end_time * 1000 > api_rooms.get_now())
-        offset = Math.round((api_rooms.get_now() - actor.vector.start_time * 1000) / 400) % Math.round(img.height / img.width) * img.width;
-    ctx.drawImage(img, 0, offset, img.width, img.width, -(width / 2), -(height / 2), width, height);
-}
-
-gui.actor_draw_funcs = {
-    'player': gui.draw_player_actor
-};
-
-gui.draw_actor = function(actor)
-{
-    gui.actor_draw_funcs[actor.actor_type](gui.ctx, actor);
 }
 
 // -------- Util functions
