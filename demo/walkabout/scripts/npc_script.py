b8bd84b4846bd35ab7621bf55ce930c544af1d9d
Ray <ray@dagon>
2018-05-17 22:02:04 +0100

    Fixing demo script

--- demo/walkabout/scripts/npc_script.py
+++ demo/walkabout/scripts/npc_script.py
@@ -12,10 +12,9 @@ def created(actor):
 
 def kickoff(actor):
     print "Running kickoff for %s" % (actor,)
-    while True:
-        log.debug("kickoff for actor %s in room %s", actor, actor.room)
-        for x, y in [(-10, -10), (10, -10), (10, 10), (-10, 10)]:
-            actor.move_wait(actor.room.position.add_coords(x, y))
-        door = random.choice(actor.room.doors)
-        actor.move_wait(door.position)
-        actor.enter(door)
+    log.debug("kickoff for actor %s in room %s", actor, actor.room)
+    for x, y in [(-10, -10), (10, -10), (10, 10), (-10, 10)]:
+        actor.move_wait(Position(x, y))
+    door = random.choice(actor.room.doors)
+    actor.move_wait(door.position)
+    actor.enter(door)
